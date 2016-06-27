346628204a1cc285b9807d9157093e877edb2d19
Felipe Trevizan <felipeptrevizan@yahoo.com.br>
2016-06-27 16:21:47 -0300

    Lab Netshoes - Felipe Trevizan
    As alterações de banco de dados estão na pasta SQL e dos códigos no próprio projeto.

---
+++ TesteImposto/SQL/TABELA_NOTA_FISCAL_ITEM.sql
@@ -0,0 +1,45 @@
+USE [Teste]
+GO
+
+/****** Object:  Table [dbo].[NotaFiscalItem]    Script Date: 06/27/2016 15:21:27 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[NotaFiscalItem](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[IdNotaFiscal] [int] NULL,
+	[Cfop] [varchar](5) NULL,
+	[TipoIcms] [varchar](20) NULL,
+	[BaseIcms] [decimal](18, 5) NULL,
+	[AliquotaIcms] [decimal](18, 5) NULL,
+	[ValorIcms] [decimal](18, 5) NULL,
+	[NomeProduto] [varchar](50) NULL,
+	[CodigoProduto] [varchar](20) NULL,
+	[BaseIpi] [decimal](18, 5) NULL,
+	[AliquotaIpi] [decimal](18, 5) NULL,
+	[ValorIpi] [decimal](18, 5) NULL,
+	[Desconto] [decimal](18, 5) NULL,
+ CONSTRAINT [PK_NotaFiscalItem] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+ALTER TABLE [dbo].[NotaFiscalItem]  WITH CHECK ADD  CONSTRAINT [FK_NotaFiscal] FOREIGN KEY([Id])
+REFERENCES [dbo].[NotaFiscalItem] ([Id])
+GO
+
+ALTER TABLE [dbo].[NotaFiscalItem] CHECK CONSTRAINT [FK_NotaFiscal]
+GO
+
--- TesteImposto/TesteImposto/FormImposto.cs
+++ TesteImposto/TesteImposto/FormImposto.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Imposto.Core.Domain;
+using Imposto.Core.Data;
 
 namespace TesteImposto
 {
@@ -19,7 +20,7 @@ namespace TesteImposto
         public FormImposto()
         {
             InitializeComponent();
-            dataGridViewPedidos.AutoGenerateColumns = true;                       
+            dataGridViewPedidos.AutoGenerateColumns = true;
             dataGridViewPedidos.DataSource = GetTablePedidos();
             ResizeColumns();
         }
@@ -32,7 +33,7 @@ namespace TesteImposto
             {
                 var coluna = dataGridViewPedidos.Columns[i];
                 coluna.Width = Convert.ToInt32(mediaWidth);
-            }   
+            }
         }
 
         private object GetTablePedidos()
@@ -42,13 +43,14 @@ namespace TesteImposto
             table.Columns.Add(new DataColumn("Codigo do produto", typeof(string)));
             table.Columns.Add(new DataColumn("Valor", typeof(decimal)));
             table.Columns.Add(new DataColumn("Brinde", typeof(bool)));
-                     
+
             return table;
         }
 
         private void buttonGerarNotaFiscal_Click(object sender, EventArgs e)
-        {            
+        {
             NotaFiscalService service = new NotaFiscalService();
+
             pedido.EstadoOrigem = txtEstadoOrigem.Text;
             pedido.EstadoDestino = txtEstadoDestino.Text;
             pedido.NomeCliente = textBoxNomeCliente.Text;
@@ -61,14 +63,20 @@ namespace TesteImposto
                     new PedidoItem()
                     {
                         Brinde = Convert.ToBoolean(row["Brinde"]),
-                        CodigoProduto =  row["Codigo do produto"].ToString(),
+                        CodigoProduto = row["Codigo do produto"].ToString(),
                         NomeProduto = row["Nome do produto"].ToString(),
-                        ValorItemPedido = Convert.ToDouble(row["Valor"].ToString())            
+                        ValorItemPedido = Convert.ToDouble(row["Valor"].ToString())
                     });
             }
 
-            service.GerarNotaFiscal(pedido);
-            MessageBox.Show("Operação efetuada com sucesso");
+            if (service.GerarNotaFiscal(pedido))
+                MessageBox.Show("Operação efetuada com sucesso");
+            else
+                MessageBox.Show("Ocorreram erros para gerar a nfe!");
+
+            txtEstadoDestino.Text = "";
+            txtEstadoOrigem.Text = "";
+            textBoxNomeCliente.Text = "";
         }
     }
 }
